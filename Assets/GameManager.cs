962842e9dc85d3505c58eb9182bf4783d7d54fb4
Nguyen Nguyen <stevennhnk113@gmail.com>
2018-09-26 20:49:01 -0700

    add menu option after gameover

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
+
+	public Menu Menu;
 
 	public void GameOver()
 	{
 		Time.timeScale = 0;
+		Menu.gameObject.SetActive(true);
+	}
+
+	public void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		Time.timeScale = 1;
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
 	}
 }
